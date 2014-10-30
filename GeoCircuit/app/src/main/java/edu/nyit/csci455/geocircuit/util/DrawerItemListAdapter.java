81bd655502fcf08919c94c613f017dd4b862ed7c
Jason Scott <jason.scott5391@gmail.com>
2014-10-30 18:47:04 -0400

    Drawer menu includes 3 options.

---
+++ GeoCircuit/app/src/main/java/edu/nyit/csci455/geocircuit/util/DrawerItemListAdapter.java
@@ -0,0 +1,78 @@
+package edu.nyit.csci455.geocircuit.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import edu.nyit.csci455.geocircuit.R;
+
+/**
+ * Created by jasonscott on 10/25/14.
+ */
+public class DrawerItemListAdapter extends BaseAdapter {
+
+    private Activity mActivity;
+    private static LayoutInflater sInflater;
+
+    public DrawerItemListAdapter(Activity activity) {
+        mActivity = activity;
+        sInflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // TODO (jasonscott) Modify to handle swapping out of fragment options.
+
+        View view = convertView;
+
+        if (view == null) {
+            view = sInflater.inflate(R.layout.item_drawer, null);
+        }
+
+        ImageView image = (ImageView) view.findViewById(R.id.item_drawer_image);
+        TextView title = (TextView) view.findViewById(R.id.item_drawer_text);
+
+        switch (position) {
+            case 0:
+                image.setImageResource(R.drawable.ic_dashboard);
+                title.setText(R.string.action_dashboard);
+                break;
+
+            case 1:
+                image.setImageResource(R.drawable.ic_circuit_manager);
+                title.setText(R.string.action_circuit_manager);
+                break;
+
+            case 2:
+                image.setImageResource(R.drawable.ic_near_me);
+                title.setText(R.string.action_near_me);
+                break;
+
+            default:
+                break;
+        }
+
+        return view;
+    }
+}
