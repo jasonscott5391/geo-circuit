faf644a297ce88944311d85e0de480a3a9b2e770
Jason Scott <jason.scott5391@gmail.com>
2014-10-30 18:42:13 -0400

    Modified package.

--- GeoCircuit/app/src/main/java/edu/nyit/csci455/geocircuit/Interface/Constants.java
+++ GeoCircuit/app/src/main/java/edu/nyit/csci455/geocircuit/Interface/Constants.java
@@ -1,10 +1,9 @@
-package app.src.main.java.edu.nyit.csci455.geocircuit.Interface;
+package edu.nyit.csci455.geocircuit.Interface;
 
 /**
  * Created by Matt on 10/26/2014.
  */
-public interface Constants
-{
+public interface Constants {
     public static final int TEXT_SIZE_SMALL = 15;
     public static final int TEXT_SIZE_LARGE = 80;
     public static final int INDEX_KM = 0;
@@ -12,7 +11,7 @@ public interface Constants
     public static final int DEFAULT_SPEED_LIMIT_MPH = 55;
     public static final int DEFAULT_SPEED_LIMIT_KM = 86;
     public static final int HOUR_MULTIPLIER = 3600;   // 1 sec=1/3600 hr
-    public static final double UNIT_MULTIPLIERS[] = { 0.001, 0.000621371192 };
+    public static final double UNIT_MULTIPLIERS[] = {0.001, 0.000621371192};
 
 
 }
--- GeoCircuit/app/src/main/java/edu/nyit/csci455/geocircuit/Interface/GPScallback.java
+++ GeoCircuit/app/src/main/java/edu/nyit/csci455/geocircuit/Interface/GPScallback.java
@@ -1,5 +1,7 @@
-package app.src.main.java.edu.nyit.csci455.geocircuit.Interface;
+package edu.nyit.csci455.geocircuit.Interface;
+
 import android.location.Location;
+
 /**
  * Created by Matt on 10/25/2014.
  */
