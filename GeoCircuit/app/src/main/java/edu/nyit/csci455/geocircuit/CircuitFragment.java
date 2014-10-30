3ba7bfaa42468192f8282b3a8ec65d91b6e5d9a6
Jason Scott <jason.scott5391@gmail.com>
2014-10-30 19:14:58 -0400

    Changed CircuitActivity to CircuitFragment

--- GeoCircuit/app/src/main/java/edu/nyit/csci455/geocircuit/CircuitFragment.java
+++ GeoCircuit/app/src/main/java/edu/nyit/csci455/geocircuit/CircuitFragment.java
@@ -1,9 +1,10 @@
 package edu.nyit.csci455.geocircuit;
 
-import android.app.Activity;
+import android.app.ListFragment;
 
 /**
  * Created by jasonscott on 10/23/14.
  */
-public class CircuitActivity extends Activity {
+public class CircuitFragment extends ListFragment {
+
 }
