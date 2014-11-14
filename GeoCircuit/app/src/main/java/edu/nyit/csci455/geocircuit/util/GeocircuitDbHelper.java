b13683b9a0f8523471b0fbdf52d03ef905adc77c
Jason Scott <jason.scott5391@gmail.com>
2014-11-14 01:40:29 -0500

    Corrected GeocircuitDbHelper.java

--- GeoCircuit/app/src/main/java/edu/nyit/csci455/geocircuit/util/GeocircuitDbHelper.java
+++ GeoCircuit/app/src/main/java/edu/nyit/csci455/geocircuit/util/GeocircuitDbHelper.java
@@ -1,11 +1,14 @@
 package edu.nyit.csci455.geocircuit.util;
 
 import android.content.Context;
+
 import net.sqlcipher.database.SQLiteOpenHelper;
 import net.sqlcipher.database.SQLiteDatabase;
 
+import edu.nyit.csci455.geocircuit.Interface.Constants;
+
 /**
- * <p>Title: GeoDbHelper.java </p>
+ * <p>Title: GeoCircuitDbHelper.java </p>
  * <p>Description:</p>
  *
  * @author jasonscott
@@ -14,9 +17,43 @@ public class GeocircuitDbHelper extends SQLiteOpenHelper {
 
     private static GeocircuitDbHelper sInstance;
 
+    private Context mContext;
+
     private final static String DATABASE_NAME = "geocircuit.db";
     private final static int DATABASE_VERSION = 1;
 
+    // SQLite create statements
+    public static final String SQL_CREATE_LOCATION_ENTRIES =
+            " ("
+                    + Constants.LOCATION_ID
+                    + Constants.INTEGER_TYPE
+                    + Constants.PRIMARY_KEY
+                    + Constants.COMMA_SEP
+                    + Constants.DATE_TIME
+                    + Constants.INTEGER_TYPE
+                    + Constants.COMMA_SEP
+                    + Constants.LATITUDE
+                    + Constants.REAL_TYPE
+                    + Constants.COMMA_SEP
+                    + Constants.LONGITUDE
+                    + Constants.REAL_TYPE
+                    + " )";
+
+    public static final String SQL_CREATE_CIRCUIT_ENTRIES =
+            " ("
+                    + Constants.CIRCUIT_ID
+                    + Constants.INTEGER_TYPE
+                    + Constants.PRIMARY_KEY
+                    + Constants.COMMA_SEP
+                    + Constants.CIRCUIT_NAME
+                    + Constants.TEXT_TYPE
+                    + Constants.COMMA_SEP
+                    + Constants.START_LOCATION
+                    + Constants.INTEGER_TYPE
+                    + Constants.COMMA_SEP
+                    + Constants.END_LOCATION
+                    + Constants.INTEGER_TYPE
+                    + " )";
 
     /**
      * @param context
@@ -35,11 +72,21 @@ public class GeocircuitDbHelper extends SQLiteOpenHelper {
      */
     private GeocircuitDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        mContext = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        SQLiteDatabase.loadLibs(mContext);
+        SQLiteDatabase database = super.getWritableDatabase(Constants.SECRET);
 
+        database.execSQL(Constants.CREATE_TABLE
+                + Constants.LOCATION_TABLE
+                + SQL_CREATE_LOCATION_ENTRIES);
+
+        database.execSQL(Constants.CREATE_TABLE
+                + Constants.CIRCUITS_TABLE
+                + SQL_CREATE_CIRCUIT_ENTRIES);
     }
 
     @Override
