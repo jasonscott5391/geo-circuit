8c1886da46626432fc53b0f1b3abcf0057d956d3
mpelle <mpelle01@nyit.edu>
2014-12-01 14:18:14 -0500

    Added adapter to link the speedometer to the dashboard's Gridview.

--- GeoCircuit/app/src/main/java/edu/nyit/csci455/geocircuit/DashboardFragment.java
+++ GeoCircuit/app/src/main/java/edu/nyit/csci455/geocircuit/DashboardFragment.java
@@ -1,9 +1,23 @@
 package edu.nyit.csci455.geocircuit;
 
+import android.content.res.Configuration;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
-
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
 /**
  * Created by jasonscott on 11/7/14.
  */
 public class DashboardFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        GridView gridView = new GridView(getActivity());
+
+
+
+        return gridView;
+    }
 }
