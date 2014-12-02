09040c36d3740add17e39a3cd136288d76110709
Jason Scott <jason.scott5391@gmail.com>
2014-12-02 16:42:39 -0500

    Added Place for NearMeFragment.

---
+++ GeoCircuit/app/src/main/java/edu/nyit/csci455/geocircuit/normalized/Place.java
@@ -0,0 +1,36 @@
+package edu.nyit.csci455.geocircuit.normalized;
+
+import android.graphics.Bitmap;
+
+public class Place {
+
+    private Bitmap icon;
+
+    private String name;
+
+    private double distance;
+
+    public Bitmap getIcon() {
+        return icon;
+    }
+
+    public void setIcon(Bitmap icon) {
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+}
